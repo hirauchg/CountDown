bb172eb973b5638958ca92f1aa261854a86dc9e9
hirauchg <gentahirauchi@gmail.com>
2019-11-12 05:42:39 +0900

    Create main menu メイン画面のメニュー実装

--- app/src/main/java/com/hirauchi/countdown/MainActivity.kt
+++ app/src/main/java/com/hirauchi/countdown/MainActivity.kt
@@ -2,11 +2,31 @@ package com.hirauchi.countdown
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            R.id.main_menu_add -> {
+
+            }
+
+            R.id.main_menu_info -> {
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
