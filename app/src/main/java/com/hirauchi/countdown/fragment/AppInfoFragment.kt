b58bf11c469a57970a35c7fc2e310a4c8cd226e4
hirauchg <gentahirauchi@gmail.com>
2019-11-12 06:48:01 +0900

    Show privacy policy, oss, app version プライバシーポリシー、オープンソースライセンス、アプリバージョンの表示実装

--- app/src/main/java/com/hirauchi/countdown/fragment/AppInfoFragment.kt
+++ app/src/main/java/com/hirauchi/countdown/fragment/AppInfoFragment.kt
@@ -1,24 +1,47 @@
 package com.hirauchi.countdown.fragment
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.hirauchi.countdown.R
 
 class AppInfoFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var mContext: Context
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        super.onCreateView(inflater, container, savedInstanceState)
         return inflater.inflate(R.layout.fragment_app_info, container, false)
+    }
+
+    override fun onAttach(context: Context){
+        mContext = context
+        super.onAttach(context)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val listView = view.findViewById<ListView>(R.id.list_view)
+        val itemList = listOf(getString(R.string.app_info_privacy), getString(R.string.app_info_oss), getString(R.string.app_info_version))
+        val adapter = ArrayAdapter<String>(mContext, android.R.layout.simple_list_item_1, itemList)
+        listView.adapter = adapter
+
+        val version = mContext.packageManager?.getPackageInfo(mContext.packageName, 0)?.versionName
+        listView.setOnItemClickListener { _, _, position, _ ->
+            when (position) {
+                0 -> startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://hirauchi-genta.com/privacy")))
+                1 -> startActivity(Intent(mContext, OssLicensesMenuActivity::class.java))
+                2 -> Toast.makeText(mContext, version, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
