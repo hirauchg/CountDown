bdf7936bbb1142941f05207a250f031c8ed5010a
hirauchg <gentahirauchi@gmail.com>
2019-11-12 06:19:12 +0900

    Change activity title 画面タイトルの変更

--- app/src/main/java/com/hirauchi/countdown/activity/AppInfoActivity.kt
+++ app/src/main/java/com/hirauchi/countdown/activity/AppInfoActivity.kt
@@ -11,6 +11,7 @@ class AppInfoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_app_info)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        title = getString(R.string.app_info_title)
     }
 
     override fun onSupportNavigateUp(): Boolean {
